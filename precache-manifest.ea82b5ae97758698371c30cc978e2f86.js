b30a6164b9dec9e418c3ae86bd2f8610f85e5c83
MaxMelnik1311 <maaxfresh1100@gmail.com>
2020-04-21 00:19:51 +0300

    Updates

---
+++ precache-manifest.ea82b5ae97758698371c30cc978e2f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7c4dfa6021786516d3a52d7a74f046",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "2957c1b4e006fc9636d7",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.a2f517e7.chunk.css"
+  },
+  {
+    "revision": "43ab2685de26c8b119fd",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.7b33b5c3.chunk.js"
+  },
+  {
+    "revision": "2957c1b4e006fc9636d7",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.bda2428b.chunk.js"
+  },
+  {
+    "revision": "49063d5db07e023fb2cb",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.4b60d742.js"
+  }
+]);
